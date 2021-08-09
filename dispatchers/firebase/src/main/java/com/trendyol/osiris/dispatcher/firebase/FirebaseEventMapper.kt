dbd18ea090dbb95c9e73500576f343314a347e77
Yasin Arslan <yasin.arslan@trendyol.com>
2021-08-09 16:16:56 +0300

    - manage type cast operations with bundleOf

--- dispatchers/firebase/src/main/java/com/trendyol/osiris/dispatcher/firebase/FirebaseEventMapper.kt
+++ dispatchers/firebase/src/main/java/com/trendyol/osiris/dispatcher/firebase/FirebaseEventMapper.kt
@@ -1,13 +1,14 @@
 package com.trendyol.osiris.dispatcher.firebase
 
 import android.os.Bundle
+import androidx.core.os.bundleOf
 
 class FirebaseEventMapper {
 
     fun map(event: FirebaseEvent): Bundle {
         val bundle = Bundle()
         event.getData().map {
-            bundle.putString(it.key, it.value.toString())
+            bundleOf(it.key to it.value)
         }
         return bundle
     }
