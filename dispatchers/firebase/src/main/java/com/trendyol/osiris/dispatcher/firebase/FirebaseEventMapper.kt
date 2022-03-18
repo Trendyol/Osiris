e592d1432fcbe3abc878c4199d8a46765d1d888e
Yasin Arslan <yasin.arslan@trendyol.com>
2022-03-18 18:03:09 +0300

    fix bug in event mapper

--- dispatchers/firebase/src/main/java/com/trendyol/osiris/dispatcher/firebase/FirebaseEventMapper.kt
+++ dispatchers/firebase/src/main/java/com/trendyol/osiris/dispatcher/firebase/FirebaseEventMapper.kt
@@ -6,10 +6,9 @@ import androidx.core.os.bundleOf
 class FirebaseEventMapper {
 
     fun map(event: FirebaseEvent): Bundle {
-        val bundle = Bundle()
-        event.getData().map {
-            bundleOf(it.key to it.value)
+        val pairs = event.getData().map {
+            it.key to it.value
         }
-        return bundle
+        return bundleOf(*pairs.toTypedArray())
     }
 }
