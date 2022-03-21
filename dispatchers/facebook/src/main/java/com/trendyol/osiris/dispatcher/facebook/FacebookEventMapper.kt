cde45c4284930dff5d108c06909437572298a931
Yasin Arslan <yasin.arslan@trendyol.com>
2022-03-21 16:56:50 +0300

    create util module and use bundleOf with map

--- dispatchers/facebook/src/main/java/com/trendyol/osiris/dispatcher/facebook/FacebookEventMapper.kt
+++ dispatchers/facebook/src/main/java/com/trendyol/osiris/dispatcher/facebook/FacebookEventMapper.kt
@@ -1,14 +1,11 @@
 package com.trendyol.osiris.dispatcher.facebook
 
 import android.os.Bundle
-import androidx.core.os.bundleOf
+import com.trendyol.osiris.util.bundleOf
 
 class FacebookEventMapper {
 
     fun map(event: FacebookEvent): Bundle {
-        val pairs = event.getData().map {
-            it.key to it.value
-        }
-        return bundleOf(*pairs.toTypedArray())
+        return bundleOf(event.getData())
     }
 }
