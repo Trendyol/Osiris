0bbc1ee4688c4d99430a448c31857a4a253ecef1
cafer.kaya <cafer.kaya@trendyol.com>
2023-01-02 10:41:00 +0300

    Support all data types for newrelic event values

--- sample/src/main/java/com/trendyol/osiris/HomeScreenSeenEvent.kt
+++ sample/src/main/java/com/trendyol/osiris/HomeScreenSeenEvent.kt
@@ -2,12 +2,14 @@ package com.trendyol.osiris
 
 import com.trendyol.osiris.dispatcher.adjustevent.OsirisAdjustEvent
 import com.trendyol.osiris.dispatcher.firebase.FirebaseEvent
+import com.trendyol.osiris.dispatcher.newrelic.NewRelicEvent
 
 class HomeScreenSeenEvent : EventBuilder {
 
     override fun build(): List<Event<EventData>> = listOf(
         buildFirebaseEvent(),
         buildAdjustEvent(),
+        buildNewRelicEvent()
     )
 
     private fun buildFirebaseEvent() = Event(
@@ -21,6 +23,13 @@ class HomeScreenSeenEvent : EventBuilder {
         name = "testEvent",
         data = OsirisAdjustEvent(
             token = ""
+        )
+    )
+
+    private fun buildNewRelicEvent() = Event(
+        name = "testEvent",
+        data = NewRelicEvent(
+            mapOf("new" to "relic", "hello" to 1)
         )
     )
 }
